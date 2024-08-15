afa0209b7dbe8bf227dc19631e0720185b7a5ac5
GobindaGoswamee6328 <gobindagoswamee@gmail.com>
2024-08-15 10:15:41 +0600

    Updated 15th August,2024

---
+++ JS Introduction/script.js
@@ -0,0 +1,19 @@
+
+alert("welcome");
+
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is correct...")
+
+var a = prompt("Enter Your Number")
+
+var istrue = confirm("are you sure that you want to leave this page")
+
+if(istrue){
+    console.log("Leave This Page...")
+}
+else{
+    console.log("Not Leaving This Page!")
+}
+
+console.log(" Your Number is " + a)
