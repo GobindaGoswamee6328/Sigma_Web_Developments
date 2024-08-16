7df7494964d779f11bd3d7a18a385c804409be85
GobindaGoswamee6328 <gobindagoswamee@gmail.com>
2024-08-16 10:49:36 +0600

    Updated 16th August,2024

---
+++ JS variable,Object and Data type/script.js
@@ -0,0 +1,36 @@
+let a = 5;
+let b = 7;
+let c = "Simanta";
+
+
+//console.log(a + b + 8)
+//console.log(typeof a,typeof b, typeof c)
+
+{
+    let a = 75;
+    console.log(a)
+}
+
+console.log(a)
+
+
+let x = " Hello ";
+let y = 32;
+let z = 3.25;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+
+let o = {
+    "name" : "Gobinda",
+    "Job Code" : 2234,
+    "is_handsome" : true
+}
+
+console.log(o)
+o.salary = "1lakh"
+console.log(o)
