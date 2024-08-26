da10f0fdce0b5ae24cc7448bc4b681679580621c
GobindaGoswamee6328 <gobindagoswamee@gmail.com>
2024-08-26 11:48:58 +0600

    Updated 26th August,2024

---
+++ Js Functions/script.js
@@ -0,0 +1,26 @@
+function nice(name) {
+    console.log("Hey " + name + " How Are You?")
+    console.log("Hey " + name + " How old Are You?")
+    console.log("Hey " + name + " what are you doing?")
+}
+
+nice("Simanta")
+
+function sum(a,b){
+    console.log(a+b)
+}
+
+sum(3,5)
+
+
+result1 = sum(2,3)
+
+console.log(" The sum is : result1")
+
+const func1 = (x)=>{
+    console.log("I am an arrow function",x)
+} 
+
+func1(34);
+func1(66);
+func1(84);
