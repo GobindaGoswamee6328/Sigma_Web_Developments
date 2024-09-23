95d31da4d53ef846e60f7182730bebbf055a0e58
GobindaGoswamee6328 <gobindagoswamee@gmail.com>
2024-09-23 09:27:42 +0600

    Updated 23rd September,2024

---
+++ Js Strings/script.js
@@ -0,0 +1,23 @@
+console.log("Welcome")
+let a = "Gobinda";
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+console.log(a[6])
+
+console.log(a.length)
+
+let real_name = "Gobinda"
+let friend = "Simanta"
+console.log(`His name is ${real_name} and his friend name is ${friend}`)
+
+let x = "Simanta"
+console.log(x.toUpperCase())
+console.log(x.toLowerCase())
+console.log(x.length)
+
+console.log(x.replace("Si","77"))
+console.log(x.concat("Goswamee"))
